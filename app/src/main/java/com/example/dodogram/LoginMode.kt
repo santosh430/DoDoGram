08656aab061e433dac3ed9630e63917e18e9479b
Santosh Bhatt <santosbhatt15@gmail.com>
2023-03-22 22:54:59 +0530

    added activity_rescue and changed colors of the theme

--- app/src/main/java/com/example/dodogram/LoginMode.kt
+++
@@ -1,4 +0,0 @@
-package com.example.dodogram
-
-enum class LoginMode {
-}
---
+++ app/src/main/java/com/example/dodogram/enums/LoginMode.kt
@@ -0,0 +1,9 @@
+package com.example.dodogram
+
+enum class LoginMode{
+    GOOGLE,
+    FACEBOOK,
+    EMAIL,
+    PHONE,
+    NONE
+}
